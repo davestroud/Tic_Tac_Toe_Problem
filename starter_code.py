0d69fd2392b3310c8ddccf5e7f124f63fed90f9d
dstroud <david@davidstroud.me>
2020-10-19 18:47:12 -0500

    add starter code

---
+++ starter_code.py
@@ -0,0 +1,16 @@
+import numpy as numpy
+import pickle
+
+BOARD_ROWS = 3
+BOARD_COLS = 3
+BOARD_SIZE = BOARD_ROWS * BOARD_COLS
+
+class State:
+    return None
+
+class Judger:
+    return None
+
+# Build an AI player
+class Player:
+    return None
--- ttt.py
+++
@@ -1,6 +0,0 @@
-import numpy as numpy
-import pickle
-
-BOARD_ROWS = 3
-BOARD_COLS = 3
-BOARD_SIZE = BOARD_ROWS * BOARD_COLS
